e28a6e730f0f817ee1ce485a03773e6b881c69b5
Joker-1212 <joker_1212@163.com>
2026-07-14 15:07:21 +0800

    完成Lab1第一次提交

---
+++ README.md
@@ -0,0 +1,19 @@
+# 第一次实验
+
+## 学生信息
+
+- 姓名：周可
+- 学号：PB25051010
+
+## 程序功能
+
+接受一次数字输入，随后输出输入的数字，本人的姓名及学号，以及 `Git Lab 1` 的字样
+
+## 运行方法
+
+```bash
+cd /path/to/directory
+g++ ./sample1.cpp -g -o sample1.out
+./sample.out
+```
+
---
+++ sample.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a << endl
+         << "PB25051010" << endl
+         << "周可" << endl
+         << "Git Lab 1" << endl;
+    return 0;
+}
+
